e5f362b8c286f1c5b5f95955a8b23ef35adf6daf
Jovanne Dela Rosa <Jovanne.DelaRosa@inchcape.com>
2022-12-27 15:02:56 +0800

    End of Section 2

---
+++ Testing/Testing.API/Data/TestingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Testing.API.Models.Domain;
+
+namespace Testing.API.Data
+{
+    public class TestingDbContext : DbContext
+    {
+        public TestingDbContext(DbContextOptions<TestingDbContext> option): base(option)
+        {
+
+        }
+
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
---
+++ Testing/Testing.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+    }
+}
